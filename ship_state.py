9e0ad2f9a40be97da62fcba0b809ef339a1c3e3e
hikeseti101@gmail.com <hikeseti101@gmail.com>
2025-11-22 14:35:57 -0800

    Fixed solve issues and made it so it include cost to return to parking in calculations.

--- ship_state.py
+++ ship_state.py
@@ -16,9 +16,45 @@ class ShipState:
 
     def __lt__(self, other):
         return self.total_cost + self.heuristic < other.total_cost + other.heuristic
+    
+    def is_goal_state(self, goal_state, container_list):
+        # Calculate the weight on the port and starboard sides
+        port_weight = 0
+        starboard_weight = 0
+        for col in range(len(self.state[0])):
+            row = 0
+            # While we don't find air/empty slot, keep going down the column
+            # This doesn't apply to Ship slots because some containers can sit on top of the Ship slots while nothing can sit on air
+            while row in range(len(self.state)) and self.state[row][col] != 1:
+                container_index = self.state[row][col]
+                if container_index > 1:  # Ignore empty and unused slots
+                    container = container_list[container_index]
+                    if col < len(self.state[0]) // 2:
+                        port_weight += container.weight
+                    else:
+                        starboard_weight += container.weight
+                row += 1
+
+        return abs(port_weight - starboard_weight) <= goal_state
 
     # Returns an 3D Numpy Array of all possible next states from the current state
-    def get_neighbors(self, visited_set):
+    def get_neighbors(self, visited_set, is_start, goal_state, container_list):
+        # print("Generating neighbors for state with total cost: ", self.total_cost)
+        # for i in range(len(self.state)):
+        #     print(container_list[self.state[i][0]].weight, container_list[self.state[i][1]].weight, container_list[self.state[i][2]].weight, container_list[self.state[i][3]].weight)
+        def get_tallest_in_between(col1, col2, top_containers):
+            if abs(col2 - col1) <= 1:
+                return 0
+            return max(top_containers[min(col1, col2)+1:max(col1, col2)])
+        
+        def get_vertical_distance(height, target_height, tallest_in_btw):
+            # standard manhattan
+            if tallest_in_btw < height or tallest_in_btw < target_height:
+                return abs(height - (target_height))
+            # raise to go over containers
+            # tallest_row_in_btw >= target_height and tallest_row_in_btw >= height
+            else:
+                return abs((tallest_in_btw + 1) - height) + abs((tallest_in_btw + 1) - target_height)
         curr_state = np.array(self.state)
         neighbors = []
         top_containers = []
@@ -33,20 +69,12 @@ class ShipState:
                 # move container if possible
                 for target_column in range(curr_state.shape[1]):
                     if column != target_column and not self.is_col_full(target_column):
-                        tallest_row_in_btw = 0
-                        if abs(target_column - column) > 1:
-                            tallest_row_in_btw = max(top_containers[min(column, target_column)+1:max(column, target_column)])
+                        # Calculate vertical and horizontal distance between two positions
+                        tallest_row_in_btw = get_tallest_in_between(column, target_column, top_containers)
                         horizontal_distance = abs(column - target_column)
-                        
                         height = top_containers[column]
                         target_height = top_containers[target_column] + 1
-                        # standard manhattan
-                        if tallest_row_in_btw < height or tallest_row_in_btw < target_height:
-                            vertical_distance = abs(height - (target_height))
-                        # raise to go over containers
-                        # tallest_row_in_btw >= target_height and tallest_row_in_btw >= height
-                        else:
-                            vertical_distance = abs((tallest_row_in_btw + 1) - height) + abs((tallest_row_in_btw + 1) - target_height)
+                        vertical_distance = get_vertical_distance(height, target_height, tallest_row_in_btw)
                         new_state_array = np.copy(curr_state)
                         # Move container from column to target_column
                         new_state_array[target_height, target_column] = new_state_array[height, column]
@@ -55,7 +83,22 @@ class ShipState:
                         if new_state_array in visited_set:
                             continue
                         move_cost = horizontal_distance + vertical_distance
+                        move_from_prev = 0
+                        if is_start:
+                            # Add extra cost for moving from parking position
+                            move_cost += column + len(self.state) - 1 - height
+                        else:
+                            # Add cost moving from last move to this column
+                            tallest_between_prev = get_tallest_in_between(self.last_move[1][1], column, top_containers)
+                            move_from_prev = abs(self.last_move[1][0] - height) + get_vertical_distance(self.last_move[1][1], column, tallest_between_prev)
+                            move_cost += move_from_prev
+                            
                         new_state = ShipState(new_state_array, self.total_cost + move_cost, self, [(height, column), (target_height, target_column)])
+                        if new_state.is_goal_state(goal_state, container_list):
+                            new_state.total_cost += (len(self.state)-1) - new_state.last_move[1][0] + new_state.last_move[1][1]
+                        print(f"Adding neighboring state with cost {vertical_distance} + {horizontal_distance} + {move_from_prev} = {move_cost}, total cost: {new_state.total_cost}")
+                        for i in range(len(new_state.state)):
+                            print(container_list[new_state.state[i][0]].weight, container_list[new_state.state[i][1]].weight, container_list[new_state.state[i][2]].weight, container_list[new_state.state[i][3]].weight)
                         neighbors.append(new_state)       
         return neighbors
     
@@ -70,7 +113,53 @@ class ShipState:
         # Check if the specified column is full
         return self.state[len(self.state)-1][column] != 1
     
-    def calc_heuristic(self):
+    def calc_heuristic(self, container_list, total_weight):
+        port_weight = 0
+        starboard_weight = 0
+        port_list = []
+        starboard_list = []
+        for col in range(len(self.state[0])):
+            row = 0
+            # While we don't find air/empty slot, keep going down the column
+            # This doesn't apply to Ship slots because some containers can sit on top of the Ship slots while nothing can sit on air
+            while row in range(len(self.state)) and self.state[row][col] != 1:
+                container_index = self.state[row][col]
+                if container_index > 1:  # Ignore empty and unused slots
+                    container = container_list[container_index]
+                    if col < len(self.state[0]) // 2:
+                        port_weight += container.weight
+                        port_list.append((container, col))
+                    else:
+                        starboard_weight += container.weight
+                        starboard_list.append((container, col))
+                row += 1
+        balance_mass = total_weight / 2
+        deficit = balance_mass - min(port_weight, starboard_weight)
+        port_heavier = port_weight > starboard_weight
+        if port_heavier:
+            port_list = sorted(port_list, key = lambda x: x[0].weight, reverse=True)
+            i = 0
+            while i < len(port_list) and port_list[i][0].weight > deficit:
+                i += 1
+            if i < len(port_list):
+                nearest_starboard = len(self.state[0]) // 2
+                while nearest_starboard < len(self.state[0]) and self.is_col_full(nearest_starboard):
+                    nearest_starboard += 1
+                self.heuristic = abs(nearest_starboard - port_list[i][1])
+            else:
+                self.heuristic = 0
+        else:
+            starboard_list = sorted(starboard_list, key = lambda x: x[0].weight, reverse=True)
+            i = 0
+            while i < len(starboard_list) and starboard_list[i][0].weight > deficit:
+                i += 1
+            if i < len(starboard_list):
+                nearest_port = len(self.state[0]) // 2 - 1
+                while nearest_port >= 0 and self.is_col_full(nearest_port):
+                    nearest_port -= 1
+                self.heuristic = abs(starboard_list[i][1] - nearest_port)
+            else:
+                self.heuristic = 0
         return
     
 if __name__ == "__main__":
--- solve.py
+++ solve.py
@@ -1,6 +1,7 @@
 import heapq
 import numpy as np
 from ship_state import Container, ShipState
+from main import read_manifest
 class ShipSolver:
     def __init__(self, start_state=None, container_list=[]):
         self.existing = set()
@@ -9,42 +10,25 @@ class ShipSolver:
         self.total_weight = np.sum([container.weight for container in container_list])
         #|Ph - Sh| <= (sum(Po, So) * 0.10)
         self.goal_state = self.total_weight * 0.10
-
-    def is_goal_state(self, ship_state):
-        # Calculate the weight on the port and starboard sides
-        port_weight = 0
-        starboard_weight = 0
-        for col in range(len(ship_state.state[0])):
-            row = 0
-            # While we don't find air/empty slot, keep going down the column
-            # This doesn't apply to Ship slots because some containers can sit on top of the Ship slots while nothing can sit on air
-            while row in range(len(ship_state.state)) and ship_state.state[row][col] != 1:
-                container_index = ship_state.state[row][col]
-                if container_index > 1:  # Ignore empty and unused slots
-                    container = self.container_list[container_index]
-                    if col < len(ship_state.state[0]) // 2:
-                        port_weight += container.weight
-                    else:
-                        starboard_weight += container.weight
-                row += 1
-        # Check if the absolute difference is within the goal state threshold
-        # print(f"Port Weight: {port_weight}, Starboard Weight: {starboard_weight}")
-        # print(f"{abs(port_weight - starboard_weight)}")
-        return abs(port_weight - starboard_weight) <= self.goal_state
     
     def solve(self):
         pq = []
         heapq.heappush(pq, self.start)
         while len(pq) != 0:
             curr_state = heapq.heappop(pq)
-            for i in range(rows):
-                print(containers[curr_state.state[i][0]].weight, containers[curr_state.state[i][1]].weight, containers[curr_state.state[i][2]].weight, containers[curr_state.state[i][3]].weight)
-            print(f"Cost: {curr_state.total_cost}")
-            if self.is_goal_state(curr_state):
+            if curr_state.state in self.existing:
+                continue
+            else:
+                self.existing.add(curr_state.state)
+            print(f"Exploring State with f(n): {curr_state.total_cost} + {curr_state.heuristic} = {curr_state.total_cost + curr_state.heuristic}")
+            for i in range(len(curr_state.state)):
+                print(self.container_list[curr_state.state[i][0]].weight, self.container_list[curr_state.state[i][1]].weight, self.container_list[curr_state.state[i][2]].weight, self.container_list[curr_state.state[i][3]].weight)
+
+            if curr_state.is_goal_state(self.goal_state, self.container_list):
                 return curr_state
-            neighbors = curr_state.get_neighbors(self.existing)
+            neighbors = curr_state.get_neighbors(self.existing, curr_state == self.start, self.goal_state, self.container_list)
             for neighbor in neighbors:
-                neighbor.calc_heuristic()
+                neighbor.calc_heuristic(self.container_list, self.total_weight)
                 heapq.heappush(pq, neighbor)
         return None
 
@@ -55,21 +39,31 @@ class ShipSolver:
         while curr_state != self.start:
             steps.append(curr_state.last_move)
             curr_state = curr_state.parent
-        # print(steps)
-        steps.append([(rows-1, 0), (steps[len(steps)-2][0])])
+        #steps.append([(rows-1, 0), (steps[len(steps)-1][0])])
         steps.reverse()
-        steps.append([(steps[len(steps)-1][1]), (rows-1, 0)])
+        #steps.append([(steps[len(steps)-1][1]), (rows-1, 0)])
+        steps2 = [(rows-1, 0)]
+        for step in steps:
+            steps2.extend(step)
+        steps2.append((rows-1, 0))
+        steps = steps2
+        for step in steps:
+            print(step)
         return steps
 
 if __name__ == "__main__":
-    containers = [Container("Ship"), Container("Unused"), Container("Ten", 10), Container("Four", 4), Container("Fourteen", 14)]
-    start = ((2, 3, 1, 1),(4, 1, 1, 1))
+    # SimpleTestCase4
+    containers = [Container("Ship"), Container("Unused"), Container("One", 6), Container("Two", 4), Container("Three", 10)]
+    start = ((1, 2, 3, 1),(1, 4, 1, 1))
+    # SimpleTestCase2
+    # containers = [Container("Ship"), Container("Unused"), Container("One", 10), Container("Two", 10), Container("Three", 10), Container("Four", 10)]
+    # start = ((2, 3, 1, 1),(4, 5, 1, 1))
+    start_state = ShipState(start, last_move = [None, (1,0)])
     rows, cols = 2, 4
-    start_state = ShipState(start)
+    # containers, start_state = read_manifest("./Given_Test_Cases/ShipCase2.txt", rows, cols)
     solver = ShipSolver(start_state, containers)
     final_state = solver.solve()
     for i in range(rows):
         print(containers[final_state.state[i][0]].weight, containers[final_state.state[i][1]].weight, containers[final_state.state[i][2]].weight, containers[final_state.state[i][3]].weight)
     print(final_state.total_cost)
     steps = solver.get_steps(final_state)
-    print(steps)
